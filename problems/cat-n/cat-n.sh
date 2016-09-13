062a5c5ba64fddbbcb58f60de1f408fd0d6a3aa8
Ken Youens-Clark <kyclark@gmail.com>
2016-09-13 08:41:52 -0700

    fixed formatting, nice solution

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
+
 function USAGE() {
-          printf "Usage:  %s   file\n" $(basename $0)
+  printf "Usage:  %s   file\n" $(basename $0)
 
-         echo "Please only input file name:"
-         exit ${1:-0}
-       }
+  echo "Please only input file name:"
+  exit ${1:-0}
+}
 
-    
-       if [[ $# -eq 0 ]]; then 
-        USAGE 1
-       fi
-
-
+if [[ $# -eq 0 ]]; then 
+  USAGE 1
+fi
 
 FILENAME=$1
-
 count=0
-
-cat $FILENAME | while read LINE
-
-do
-
-let count++
-
-echo "$count $LINE"
-
+cat $FILENAME | while read LINE; do
+  let count++
+  echo "$count $LINE"
 done
-
-
-
-
-
